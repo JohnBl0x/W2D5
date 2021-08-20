090f03373fff1434fa06ec5f53fdf939a8e916fc
John Blox <88888689+JohnBl0x@users.noreply.github.com>
2021-08-20 12:51:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let x = "John"
+let y = "Doe"
+console.log(x + " <> " + y)
+
+let myInfo = {
+    name: "John",
+    surname: "Blox",
+    email: "johnblox5@gmail.com"
+}
+delete myInfo.email
+console.log(myInfo)
+
+let myArray = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+console.log(myArray)
+
+for (i=0;i<10;i++) {
+    let myArray = []
+    let randomNumber = Math.floor(Math.random() * 10)
+    myArray.push(randomNumber)
+    console.log(myArray)
+}
+
